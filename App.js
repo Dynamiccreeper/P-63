c0384950a72a3e13c5c2ae3bc50b0f9e7c1339f9
Dynamiccreeper <74767888+Dynamiccreeper@users.noreply.github.com>
2021-06-30 18:04:03 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,19 +1,33 @@
-import * as React from 'react';
-import { Text, View, StyleSheet } from 'react-native';
-import Constants from 'expo-constants';
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import HomeScreen from './screens/HomeScreen'
+import {createSwitchNavigator, createAppContainer} from 'react-navigation'
 
-// You can import from local files
-import AssetExample from './components/AssetExample';
+export default class App extends React.Component {
+  render(){
+    return (
+      <View style={styles.container}>
+       <AppContainer/>
+      </View>
+    );
+  }
 
-// or any pure javascript modules available in npm
-import { Card } from 'react-native-paper';
-
-export default function App() {
-  return (
-    <View>
-     
-    </View>
-  );
 }
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+var AppNavigator= createSwitchNavigator({
+  HomeScreen: HomeScreen,
+
+})
+
+var AppContainer=createAppContainer(AppNavigator)
 
